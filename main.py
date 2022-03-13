566abc4eaec1a9b624d43f9149e5d6935ca2b869
Gitea <gitea@fake.local>
2022-03-13 20:58:54 +0530

    Added message sending service on checking the weather for next 12 hours.

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 import requests
+from twilio.rest import Client
 
 api_key = "71dbe179ffa19771c15ad090befe3da4"
-MY_LAT = 13.00 # Your latitude
-MY_LONG = 77.583333 # Your longitude
+# MY_LAT = 13.00  # Your latitude
+# MY_LONG = 77.583333  # Your longitude
+MY_LAT = 43.610767  # Test Paris latitude
+MY_LONG = 3.876716  # Test Paris longitude
+account_sid = "ACf21e6506da5233d77a936d156f5890db"
+auth_token = "10d858220369bb30fc736fe4e1ab6327"
+PHONE_NUM = "+17627603600"
 
-parameters ={
+parameters = {
     "lat": MY_LAT,
     "lon": MY_LONG,
     "appid": api_key,
+    "exclude": "current,minutely,daily"
 }
 
 api_url = "https://api.openweathermap.org/data/2.5/onecall"
 
-response = requests.get(url=api_url, params=parameters)
+response = requests.get(api_url, params=parameters)
 response.raise_for_status()
-print(response.status_code)
-data = response.json()
+# print(response.status_code)
+weather_data = response.json()
+will_rain = False
+weather_slice = weather_data["hourly"][:12]
 
-print(data["hourly"])
+for each in weather_slice:
+    weather_condition_code = each['weather'][0]['id']
+    if weather_condition_code < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔.",
+        from_=PHONE_NUM,
+        to='+919886605789'
+    )
+    print(message.status)
